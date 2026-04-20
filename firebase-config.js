3f60af22cd84578514050a49dc0b781b60fee92c
Yaakov114 <58630244+Yaakov114@users.noreply.github.com>
2026-04-20 17:45:38 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "כאן תדביק",
+  authDomain: "כאן תדביק",
+  projectId: "כאן תדביק",
+  storageBucket: "כאן תדביק",
+  messagingSenderId: "כאן תדביק",
+  appId: "כאן תדביק"
+};
